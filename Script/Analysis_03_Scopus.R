70675cc1192e0fc27a7d3ed7f0e1a6a339cb8064
andreaspacher <75671403+andreaspacher@users.noreply.github.com>
2021-04-24 14:19:13 +0200

    Update Analysis_03_Scopus.R

--- Script/Analysis_03_Scopus.R
+++ Script/Analysis_03_Scopus.R
@@ -1,17 +1,7 @@
-#
-# EDIT THESE DATA
-#
-fileread <- "./Data/02_Scopus.xlsx"
-fileToSave <- "./Output/all-journals-scopus.csv"
-
-#
-# EXECUTABLE CODE STARTS HERE
-#
-
-scopusdata <- readxl::read_xlsx(fileread)
+scopusdata <- readxl::read_xlsx("Data\\02_Scopus.xlsx")
 scopus_publishers <- scopusdata[, 23]
 
 df <- data.frame(table(scopus_publishers))
 df <- df[rev(order(df$Freq)), ]
 
-write.csv(df, fileToSave, row.names = FALSE)
+write.csv(df, "Output\\03_publishers_Scopus.csv", row.names = FALSE)
