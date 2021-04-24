567caf6954aa6f12720ee4dd2cb5b9f9f4d2dd5e
andreaspacher <ap@ooir.org>
2021-04-24 14:11:31 +0200

    rename files

--- Script/Analysis_05_Merge-and-Harmonize-Publishers.R
+++ Script/Analysis_05_Merge-and-Harmonize-Publishers.R
@@ -1,25 +1,24 @@
 #
 # IMPORT DATA
 #
-filePath <- "C:\\Your\\File\\Path\\"
-
-csv_doaj <- read.delim(file = paste0(filePath, "all-journals-doaj.csv"), header = TRUE, sep = ",")
-csv_publons <- read.delim(file = paste0(filePath, "all-journals-publons.csv"), header = TRUE, sep = ",")
-csv_romeo <- read.delim(file = paste0(filePath, "all-journals-sherparomeo.csv"), header = TRUE, sep = ",")
-csv_scopus <- read.delim(file = paste0(filePath, "all-journals-scopus.csv"), header = TRUE, sep = ",")
+library(readr)
+csv_doaj <- read_csv(file = "Output\\01_publishers_DOAJ.csv")
+csv_publons <- read_csv(file = "Output\\02_publishers_Publons.csv")
+csv_scopus <- read_csv(file = "Output\\03_publishers_Scopus.csv")
+csv_romeo <- read_csv(file = "Output\\04_publishers_SherpaRomeo.csv")
 
 names(csv_doaj) <- c("Publisher", "Journals")
 names(csv_publons) <- c("Publisher", "Journals", "Reviews")
-names(csv_romeo) <- c("Publisher", "Journals")
 names(csv_scopus) <- c("Publisher", "Journals")
+names(csv_romeo) <- c("Publisher", "Journals")
 
 # ============================
 # Preliminary Results
 # ============================
 #
-# DOAJ      7217 Publishers
-# Publons   430 Publishers
-# Romeo     4611 Publishers
+# DOAJ       7217 Publishers
+# Publons     430 Publishers
+# Romeo      4611 Publishers
 # Scopus    11881 Publishers
 #
 
@@ -34,7 +33,9 @@ alljournals <- list(
 
 # How many journals should a publisher have in order to be listed?
 # This example shows a threshold of 30 journals per publisher
-alljournals <- lapply(alljournals, function(x) subset(x, Journals >= 30))
+threshold <- 50
+
+alljournals <- lapply(alljournals, function(x) subset(x, Journals >= threshold))
 
 csv_doaj <- as.data.frame(alljournals[1])
 csv_publons <- as.data.frame(alljournals[2])
@@ -48,13 +49,12 @@ names(csv_scopus) <- c("Publisher", "Scopus_Journals")
 
 doaj_publons <- merge(csv_doaj, csv_publons, by = "Publisher", all = T)
 romeo_scopus <- merge(csv_romeo, csv_scopus, by = "Publisher", all = T)
-
 alljournaldata <- merge(doaj_publons, romeo_scopus, by = "Publisher", all = T)
 
 # harmonize publisher names
-# you can add additional names to harmonize into "harmonize-publishers-data.txt"
+# you can add additional names to harmonize into "03_publishers_harmonization.txt"
 # separated by tabs (\t) (first, the deviation, then, second, after the tab, the harmonization)
-harmonia <- read.delim(file = paste0(filePath, "harmonize-publishers-data.txt"), header = TRUE, sep = "\t")
+harmonia <- read.delim(file = "Data\\03_publishers_harmonization.txt", header = TRUE, sep = "\t")
 
 for (i in 1:nrow(harmonia)) {
   alljournaldata$Publisher[alljournaldata$Publisher == harmonia$Deviation[i]] <- harmonia$Harmonization[i]
@@ -74,4 +74,5 @@ DT <- DT[order(Publisher)]
 DT <- dplyr::mutate(DT, maxjournals = pmax(DOAJ_Journals, Publons_Journals, Romeo_Journals, Scopus_Journals))
 DT <- DT[order(-maxjournals)]
 
-write.csv(DT, paste0(filePath, "allpublishers.csv"), row.names = FALSE)
+currdate <- Sys.Date()
+write.csv(DT, paste0("Output\\allpublishers-", currdate, ".csv"), row.names = FALSE)
